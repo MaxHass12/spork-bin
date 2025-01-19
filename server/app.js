420ac30dc436d0f1d93069722f88d5954434d75f
Ping Honzay <phonzay1@gmail.com>
2025-01-19 13:26:40 -0600

    GET routes for all bins and new bin IDs

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const apiRouter = require('./routes/api');
 const app = express();
 
 app.use(express.json());
+// mount the apiRouter to the '/api' path
 app.use('/api', apiRouter);
 // how do we want to include/serve static front-end files?
 app.use(express.static('dist'));
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,7 +1,55 @@
-const express = require('express');
-const router = express.Router();
-// require functions from './config/db to connect to MongoDB and PostgreSQL
+const apiRouter = require('express').Router();
+// require functions from './config/db' to connect to MongoDB and PostgreSQL
 
-// invoke functions from './config/db to connect to MongoDB and PostgreSQL
+// invoke functions from './config/db' to connect to MongoDB and PostgreSQL
+
+// ASCII codes for digits are 48-57
+// ASCII codes for lowercase letters are 97-122
+function getRandomChar() {
+  let asciiCode;
+
+  if (Math.random() < 0.4) {
+    asciiCode = Math.floor(Math.random() * 10 + 48); 
+  } else {
+    asciiCode = Math.floor(Math.random() * 26 + 97); 
+  }
+
+  return String.fromCharCode(asciiCode);
+}
+
+function createNewBinID() {
+  let binID = '';
+
+  for (let i = 0; i <= 7; i++) {
+    binID += getRandomChar();
+  }
+
+  return binID;
+}
+
+function binIdInUse(binID) {
+  // check if `binID` already exists in the PG `bins` table
+}
 
 // GET /api/bins
+apiRouter.get('/bins', async (req, res) => {
+  try {
+    // replace pgQueries.getAllBins() with PG method from './config/db'
+    const bins = await pgQueries.getAllBins();
+    res.json(bins);
+  } catch (error) {
+    res.status(500).send("Something went wrong - couldn't get bins");
+  }
+});
+
+// GET /api/new_bin_id (to get a new random_id)
+apiRouter.get('/new_bin_id', async (req, res) => {
+  let newBinID = createNewBinID();
+  // implement `binIdInUse` function to talk to PG database
+  while (binIdInUse(newBinID)) {
+    newBinID = createNewBinID();
+  }
+  res.send(newBinID);
+});
+
+module.exports = apiRouter;
