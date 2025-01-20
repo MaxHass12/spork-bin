349c47f70f7ab2e8aee55c65370e6b21f5e8071a
kjpoccia <kjpsubs@gmail.com>
2025-01-19 22:41:06 -0500

    mongo model

---
+++ server/models/mongoRequest.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const PayloadSchema = new mongoose.Schema({
+    request_id: { type: Number, required: true },
+    payload: { type: String },
+});
+
+module.exports = mongoose.model('MongoPayload', PayloadSchema);
