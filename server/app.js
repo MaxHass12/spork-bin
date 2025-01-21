68a1b01af7b9ecebc3667977808d33e92bf2bfe1
Shwetank Tewari <shwetank@Shwetanks-MacBook-Pro.local>
2025-01-21 15:46:13 -0500

    able to serve react app from /view/*

--- server/app.js
+++ server/app.js
@@ -4,10 +4,13 @@ const apiRouter = require('./routes/api');
 const binsRouter = require('./routes/bins');
 const app = express();
 
+app.use(express.static('dist'));
+app.use('/view/*', express.static('dist'));
+
 app.use(express.json());
+
 app.use('/api', apiRouter);
 app.use('/bins', binsRouter);
-app.use(express.static('dist'));
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
